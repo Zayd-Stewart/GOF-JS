15242d19a9ba29f0be64b35e23ae1c60a50f3592
Zayd-Stewart <121957911+Zayd-Stewart@users.noreply.github.com>
2023-01-05 17:46:13 +0200

    create GOF

---
+++ precache-manifest.8c5c5692fa09f62dc4734c7ae37e2ab8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afff044d46dbe6e20d273dd31e4e01f1",
+    "url": "/the-game-of-life/index.html"
+  },
+  {
+    "revision": "043077b4f80dd6e8ec63",
+    "url": "/the-game-of-life/static/css/2.33bbdc9a.chunk.css"
+  },
+  {
+    "revision": "affedf1ee222269ced39",
+    "url": "/the-game-of-life/static/css/main.3c37ab5d.chunk.css"
+  },
+  {
+    "revision": "043077b4f80dd6e8ec63",
+    "url": "/the-game-of-life/static/js/2.27d6ef2b.chunk.js"
+  },
+  {
+    "revision": "affedf1ee222269ced39",
+    "url": "/the-game-of-life/static/js/main.2621957f.chunk.js"
+  },
+  {
+    "revision": "d67aa138d7b6b460c2de",
+    "url": "/the-game-of-life/static/js/runtime~main.bb4134ae.js"
+  }
+]);
